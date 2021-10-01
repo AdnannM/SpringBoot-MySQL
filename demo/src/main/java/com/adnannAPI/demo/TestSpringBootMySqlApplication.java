c193b98dfe96c565fb76bf940f41c63ddb869de9
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-01 10:38:35 +0200

    Setup SpringBoot MySQL

---
+++ demo/src/main/java/com/adnannAPI/demo/TestSpringBootMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.adnannAPI.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootMySqlApplication.class, args);
+	}
+
+}
